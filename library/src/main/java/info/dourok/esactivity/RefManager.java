7b189ec3389bb94a1995bb5d3371e35e89f0845e
Tiou Lims <dourokinga@gmail.com>
2018-02-23 15:05:30 +0800

    整合 qa

--- library/src/main/java/info/dourok/esactivity/RefManager.java
+++ library/src/main/java/info/dourok/esactivity/RefManager.java
@@ -10,7 +10,7 @@ import java.util.Map;
 
 /**
  * 用于通过 Map 在 Activity 之间直接传递引用 每个 RefMap 都会和 Intent 绑定在一起 Intent 存放着 RefMap 在 {@link RefManager} 中的
- * Key
+ * Key。
  *
  * @author tiaolins
  * @date 2017/8/31
@@ -28,6 +28,7 @@ public class RefManager {
     return get(context.getIntent(), key);
   }
 
+  
   public <T> T get(Intent intent, String key) {
     Map<String, Object> refMap = getRefMap(intent);
     if (refMap != null) {
@@ -47,12 +48,24 @@ public class RefManager {
     refMap.put(key, value);
   }
 
-  /** 每个 buidler 实例都有独立的 ReFMap */
+  /** 每个 buidler 实例都有独立的 ReFMap.*/
   public @NonNull Map<String, Object> getOrCreateRefMap(BaseBuilder builder) {
     return getOrCreateRefMap(builder.asIntent().getIntent());
   }
 
-  /** @return 返回 RefMap 的 key，如果 Intent 未绑定 RefMap，则返回 0 */
+  /** 用于 setResult 的 Intent。 */
+  public @NonNull Map<String, Object> getOrCreateRefMap(Intent intent) {
+    Map<String, Object> refMap = getRefMap(intent);
+    if (refMap == null) {
+      refMap = new HashMap<>();
+      int keyOfMap = intent.hashCode();
+      intent.putExtra(KEY_OF_MAP, keyOfMap);
+      sGlobalRefMap.put(keyOfMap, refMap);
+    }
+    return refMap;
+  }
+
+  /** 返回 RefMap 的 key，如果 Intent 未绑定 RefMap，则返回 0 */
   public static int getKeyOfMap(Intent intent) {
     return intent.getIntExtra(KEY_OF_MAP, 0);
   }
@@ -64,27 +77,12 @@ public class RefManager {
   /** 如果 oldIntent 绑定了 RefMap，那么将其解绑并重新绑定到 newIntent */
   public void rebindRefMap(Intent oldIntent, Intent newIntent) {
     if (hasRefMap(oldIntent)) {
-      Map<String, Object> refMap = getRefMap(oldIntent);
-
       sGlobalRefMap.remove(getKeyOfMap(oldIntent));
       oldIntent.removeExtra(KEY_OF_MAP);
-
       int keyOfMap = newIntent.hashCode();
       newIntent.putExtra(KEY_OF_MAP, keyOfMap);
-      sGlobalRefMap.put(keyOfMap, refMap);
+      sGlobalRefMap.put(keyOfMap, getRefMap(oldIntent));
     }
-  }
-
-  /** 用于 setResult 的 Intent */
-  public @NonNull Map<String, Object> getOrCreateRefMap(Intent intent) {
-    Map<String, Object> refMap = getRefMap(intent);
-    if (refMap == null) {
-      refMap = new HashMap<>();
-      int keyOfMap = intent.hashCode();
-      intent.putExtra(KEY_OF_MAP, keyOfMap);
-      sGlobalRefMap.put(keyOfMap, refMap);
-    }
-    return refMap;
   }
 
   public @Nullable Map<String, Object> getRefMap(Intent intent) {
@@ -126,6 +124,10 @@ public class RefManager {
     }
   }
 
+  public int getInt(Activity context, String key, int defaultValue) {
+    return getInt(context.getIntent(), key, defaultValue);
+  }
+
   public char getChar(Intent intent, String key, char defaultValue) {
     Map<String, Object> refMap = getRefMap(intent);
     if (refMap != null) {
@@ -138,6 +140,10 @@ public class RefManager {
     } else {
       return defaultValue;
     }
+  }
+
+  public char getChar(Activity context, String key, char defaultValue) {
+    return getChar(context.getIntent(), key, defaultValue);
   }
 
   public short getShort(Intent intent, String key, short defaultValue) {
@@ -154,6 +160,10 @@ public class RefManager {
     }
   }
 
+  public short getShort(Activity context, String key, short defaultValue) {
+    return getShort(context.getIntent(), key, defaultValue);
+  }
+
   public byte getByte(Intent intent, String key, byte defaultValue) {
     Map<String, Object> refMap = getRefMap(intent);
     if (refMap != null) {
@@ -166,6 +176,10 @@ public class RefManager {
     } else {
       return defaultValue;
     }
+  }
+
+  public byte getByte(Activity context, String key, byte defaultValue) {
+    return getByte(context.getIntent(), key, defaultValue);
   }
 
   public long getLong(Intent intent, String key, long defaultValue) {
@@ -182,6 +196,10 @@ public class RefManager {
     }
   }
 
+  public long getLong(Activity context, String key, long defaultValue) {
+    return getLong(context.getIntent(), key, defaultValue);
+  }
+
   public double getDouble(Intent intent, String key, double defaultValue) {
     Map<String, Object> refMap = getRefMap(intent);
     if (refMap != null) {
@@ -194,6 +212,10 @@ public class RefManager {
     } else {
       return defaultValue;
     }
+  }
+
+  public double getDouble(Activity context, String key, double defaultValue) {
+    return getDouble(context.getIntent(), key, defaultValue);
   }
 
   public float getFloat(Intent intent, String key, float defaultValue) {
@@ -210,6 +232,10 @@ public class RefManager {
     }
   }
 
+  public float getFloat(Activity context, String key, float defaultValue) {
+    return getFloat(context.getIntent(), key, defaultValue);
+  }
+
   public boolean getBoolean(Intent intent, String key, boolean defaultValue) {
     Map<String, Object> refMap = getRefMap(intent);
     if (refMap != null) {
@@ -222,34 +248,6 @@ public class RefManager {
     } else {
       return defaultValue;
     }
-  }
-
-  public int getInt(Activity context, String key, int defaultValue) {
-    return getInt(context.getIntent(), key, defaultValue);
-  }
-
-  public char getChar(Activity context, String key, char defaultValue) {
-    return getChar(context.getIntent(), key, defaultValue);
-  }
-
-  public short getShort(Activity context, String key, short defaultValue) {
-    return getShort(context.getIntent(), key, defaultValue);
-  }
-
-  public byte getByte(Activity context, String key, byte defaultValue) {
-    return getByte(context.getIntent(), key, defaultValue);
-  }
-
-  public long getLong(Activity context, String key, long defaultValue) {
-    return getLong(context.getIntent(), key, defaultValue);
-  }
-
-  public double getDouble(Activity context, String key, double defaultValue) {
-    return getDouble(context.getIntent(), key, defaultValue);
-  }
-
-  public float getFloat(Activity context, String key, float defaultValue) {
-    return getFloat(context.getIntent(), key, defaultValue);
   }
 
   public boolean getBoolean(Activity context, String key, boolean defaultValue) {
